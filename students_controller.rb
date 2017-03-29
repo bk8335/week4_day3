7591c99d74c418489837edec5f9aa8a2f8a9923e
Benjamin <benjamin.kinnard1@gmail.com>
2017-03-29 13:53:15 +0100

    added the new student form and post method with links

--- students_controller.rb
+++ students_controller.rb
@@ -7,3 +7,13 @@ get '/students' do
 @students = Student.all()
 erb(:index)
   end
+
+get '/students/new' do
+  erb(:new)
+end
+
+post '/students' do
+  @students = Student.new(params)
+  @students.save()
+  erb(:student_confirmation)
+end
